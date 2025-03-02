eefc9ff3e850a569172655a6eff0bfdf2f8fa417
Ayan Khan <91457435+Zakk242K@users.noreply.github.com>
2025-03-02 17:14:27 +0530

    did the ful cde for quad eqn.

--- P9.cpp
+++ P9.cpp
@@ -1,1 +1,43 @@
-//23-11-24 
+//23-11-24 (Date of experiment)
+//This is a simple program used for calculating the roots of the quadratic equation.
+
+using namespace std;
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, x1, x2, D;
+    cout << "Assignment 9- Roots of Quadratic Equation" << endl;
+
+    cout << "Enter the value of a: " << endl;
+    cin >> a;
+
+    cout << "Enter the value of b: " << endl;
+    cin >> b;
+
+    cout << "Enter the value of c: " << endl;
+    cin >> c;
+
+    D = b * b - 4 * a * c;
+
+    if (D > 0)
+    {
+        x1 = (-b + sqrt(D)) / (2 * a);
+        x2 = (-b - sqrt(D)) / (2 * a);
+        cout << "The roots are real and different." << endl;
+        cout << "The roots are: " << x1 << " and " << x2 << endl;
+    }
+    else if (D == 0)
+    {
+        x1 = -b / (2 * a);
+        cout << "The roots are real and same." << endl;
+        cout << "The roots are: " << x1 << " and " << x1 << endl;
+    }
+    else
+    {
+        cout << "The roots are imaginary." << endl;
+    }
+
+    return 0;
+}
